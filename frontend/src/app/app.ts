fbc69cc40a8f03fc57af6ecf421bdf6e02ff43c3
Van <84937597+vandalit@users.noreply.github.com>
2025-09-17 20:43:04 -0300

    ajuste tokens colores

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   // Styles handled by global SCSS system
 })
